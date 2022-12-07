4e13dbaa4e03f28d806933ddd7347dac535f7820
Fnote Mehari <fnote.md@gmail.com>
2022-12-07 10:22:50 +0300

    test

--- app/Http/Controllers/productsController.php
+++ app/Http/Controllers/productsController.php
@@ -218,12 +218,12 @@ class productsController extends Controller
         $fileNameToStore = $filename.'_'.time().'.'.$extension;
         //upload Image
         //$realPath = 'C:\wamp64\www\roadmap\public\storage\products\\';
-        $realPath = 'https://rom.signumdev.com/storage/products/';
+        $realPath = '/home/signumdevcom/rom/storage/app/public/products/';
         //$realPath = public_path().'\storage\products\\';
         //$realPath = storage_path().'/app/public/products/';
         //$thumbnailPath = public_path().'\storage\productsThumb\\';
         //$thumbnailPath = 'C:\wamp64\www\roadmap\public\storage\productsThumb\\';
-        $thumbnailPath= 'https://rom.signumdev.com/storage/productsThumb/';
+        $thumbnailPath= '/home/signumdevcom/rom/storage/app/public/productsThumb/';
         //$thumbnailPath = storage_path().'/app/public/productsThumb/';
         $thumbnailImage->save($realPath.$fileNameToStore);
 
